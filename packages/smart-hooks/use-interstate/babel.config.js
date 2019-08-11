2fd9c1655bce7fde3fce586431833fbfccfb490a
Andrei Kovalev <turtlefly@gmail.com>
2019-08-11 01:21:25 -0400

    Migrate from .babelrc to babel.config.js

---
+++ packages/smart-hooks/use-interstate/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['last 2 versions', 'safari >= 7'],
+        },
+        modules: false,
+      },
+      '@babel/preset-react',
+    ],
+  ],
+};
