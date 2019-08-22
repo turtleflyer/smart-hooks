25c65770f24e5c961a7409e97fee920b44e866ce
Andrei Kovalev <turtlefly@gmail.com>
2019-08-22 03:44:36 -0400

    Revert "Add incCount and decCount methods to store"
    This reverts commit e3c9ce95fa8d6250c358d4c8360e4d82ad3a8662.

--- src/utils/getStore.js
+++ src/utils/getStore.js
@@ -6,23 +6,7 @@ const getStore = () => {
     storeStructure.map.get(id) !== undefined || storeStructure.map.set(id, { setters: [] })
   )
     && storeStructure.map.get(id);
-
-  const set = (id, v) => storeStructure.map.set(id, v);
-
-  const incCounter = (id) => {
-    const entry = get(id);
-    set(id, { ...entry, counter: (entry.counter || 0) + 1 });
-  };
-
-  const decCounter = (id) => {
-    const entry = get(id);
-    const nextCounter = entry.counter - 1;
-    if (nextCounter > 0) {
-      set(id, { ...entry, counter: nextCounter });
-    } else {
-      storeStructure.map.clear(id);
-    }
-  };
+  const set = (id, value) => storeStructure.map.set(id, value);
 
   // eslint-disable-next-line no-underscore-dangle
   const _getMapsForTesting = () => storeStructure;
@@ -30,8 +14,6 @@ const getStore = () => {
   return {
     get,
     set,
-    incCounter,
-    decCounter,
     _getMapsForTesting,
   };
 };
