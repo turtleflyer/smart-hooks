01dabd65ddb3abfac992fa66870c091d3f51390f
Andrei Kovalev <turtlefly@gmail.com>
2019-08-24 02:52:45 -0400

    Add secondary API in the same tests

--- packages/smart-hooks/use-interstate/__tests__/useInterstate.1.test.js
+++ packages/smart-hooks/use-interstate/__tests__/useInterstate.1.test.js
@@ -3,238 +3,249 @@
 
 import React from 'react';
 
-describe('Test useInterstate functionality', () => {
-  let render;
-  let getLastMaps;
-  let CanListen;
-  let CanUpdate;
-  let CanListenAndUpdate;
+const siblingsCanCommunicate = imports => () => {
+  const {
+    CanUpdate, CanListen, render, getLastMaps,
+  } = imports;
+  const subscribeId = '1';
+  const testId1 = 'updater';
+  const testId2 = 'listener';
+  const countRender1 = jest.fn();
+  const countRender2 = jest.fn();
+  const TestComponent = () => (
+    <>
+      <CanUpdate
+        {...{
+          subscribeId,
+          testId: testId1,
+          countRender: countRender1,
+        }}
+      />
+      <CanListen
+        {...{
+          subscribeId,
+          testId: testId2,
+          countRender: countRender2,
+        }}
+      />
+    </>
+  );
 
-  beforeEach(() => {
-    jest.isolateModules(() => {
-      ({
-        render,
-        getLastMaps,
-        // eslint-disable-next-line global-require
-      } = require('./prerequisite'));
+  const { unmount, fireNode, getTextFromNode } = render(<TestComponent />);
+  const maps = getLastMaps();
+  expect(getTextFromNode(testId2)).toBe('');
+  fireNode(testId1, 'n');
+  expect(getTextFromNode(testId2)).toBe('n');
+  expect(countRender1).toHaveBeenCalledTimes(1);
+  expect(countRender2).toHaveBeenCalledTimes(2);
+  expect(maps.map.get(subscribeId).setters.length).toBe(1);
+  unmount();
+  expect(maps.map.get(subscribeId).setters.length).toBe(0);
+};
 
-      ({
-        CanListen,
-        CanUpdate,
-        CanListenAndUpdate,
-        // eslint-disable-next-line global-require
-      } = require('./testComponentGeneralAPI'));
-    });
-  });
-
-  test('siblings can communicate', () => {
-    const subscribeId = '1';
-    const testId1 = 'updater';
-    const testId2 = 'listener';
-    const countRender1 = jest.fn();
-    const countRender2 = jest.fn();
-    const TestComponent = () => (
-      <>
-        <CanUpdate
+const sophisticatedStructure = imports => () => {
+  const {
+    render, getLastMaps, CanListen, CanUpdate, CanListenAndUpdate,
+  } = imports;
+  const subscribeId1 = '1';
+  const subscribeId2 = '2';
+  const testId1 = 'first';
+  const testId2 = 'second';
+  const testId3 = 'third';
+  const testId4 = 'forth';
+  const testId5 = 'fifth';
+  const testId6 = 'sixth';
+  const testId7 = 'seventh';
+  const testId8 = 'eighth';
+  const testId9 = 'ninth';
+  const altComposeCallback = set => ({ target: { value } }) => {
+    set(old => (old || '') + value);
+  };
+  const countRender1 = jest.fn();
+  const countRender2 = jest.fn();
+  const countRender3 = jest.fn();
+  const countRender4 = jest.fn();
+  const countRender5 = jest.fn();
+  const countRender6 = jest.fn();
+  const countRender7 = jest.fn();
+  const countRender8 = jest.fn();
+  const countRender9 = jest.fn();
+  const TestComponent = () => (
+    <>
+      <CanListen
+        {...{
+          subscribeId: subscribeId1,
+          testId: testId1,
+          countRender: countRender1,
+        }}
+      >
+        <CanListenAndUpdate
           {...{
-            subscribeId,
-            testId: testId1,
-            countRender: countRender1,
-          }}
-        />
-        <CanListen
-          {...{
-            subscribeId,
+            subscribeId: subscribeId2,
             testId: testId2,
             countRender: countRender2,
           }}
         />
-      </>
-    );
-
-    const { unmount, fireNode, getTextFromNode } = render(<TestComponent />);
-    const maps = getLastMaps();
-    expect(getTextFromNode(testId2)).toBe('');
-    fireNode(testId1, 'n');
-    expect(getTextFromNode(testId2)).toBe('n');
-    expect(countRender1).toHaveBeenCalledTimes(1);
-    expect(countRender2).toHaveBeenCalledTimes(2);
-    expect(maps.map.get(subscribeId).setters.length).toBe(1);
-    unmount();
-    expect(maps.map.get(subscribeId).setters.length).toBe(0);
-  });
-
-  test('sophisticated structure can communicate', () => {
-    const subscribeId1 = '1';
-    const subscribeId2 = '2';
-    const testId1 = 'first';
-    const testId2 = 'second';
-    const testId3 = 'third';
-    const testId4 = 'forth';
-    const testId5 = 'fifth';
-    const testId6 = 'sixth';
-    const testId7 = 'seventh';
-    const testId8 = 'eighth';
-    const testId9 = 'ninth';
-    const altComposeCallback = set => ({ target: { value } }) => {
-      set(old => (old || '') + value);
-    };
-    const countRender1 = jest.fn();
-    const countRender2 = jest.fn();
-    const countRender3 = jest.fn();
-    const countRender4 = jest.fn();
-    const countRender5 = jest.fn();
-    const countRender6 = jest.fn();
-    const countRender7 = jest.fn();
-    const countRender8 = jest.fn();
-    const countRender9 = jest.fn();
-    const TestComponent = () => (
-      <>
+      </CanListen>
+      <div>
         <CanListen
           {...{
             subscribeId: subscribeId1,
-            testId: testId1,
-            countRender: countRender1,
+            testId: testId3,
+            countRender: countRender3,
           }}
-        >
+        />
+        <div>
           <CanListenAndUpdate
             {...{
-              subscribeId: subscribeId2,
-              testId: testId2,
-              countRender: countRender2,
+              subscribeId: subscribeId1,
+              composeCallback: altComposeCallback,
+              testId: testId4,
+              countRender: countRender4,
             }}
           />
-        </CanListen>
-        <div>
           <CanListen
             {...{
-              subscribeId: subscribeId1,
-              testId: testId3,
-              countRender: countRender3,
+              subscribeId: subscribeId2,
+              testId: testId5,
+              countRender: countRender5,
             }}
           />
           <div>
-            <CanListenAndUpdate
-              {...{
-                subscribeId: subscribeId1,
-                composeCallback: altComposeCallback,
-                testId: testId4,
-                countRender: countRender4,
-              }}
-            />
-            <CanListen
+            <CanUpdate
               {...{
                 subscribeId: subscribeId2,
-                testId: testId5,
-                countRender: countRender5,
+                composeCallback: altComposeCallback,
+                testId: testId6,
+                countRender: countRender6,
               }}
-            />
-            <div>
-              <CanUpdate
+            >
+              <CanListenAndUpdate
                 {...{
                   subscribeId: subscribeId2,
                   composeCallback: altComposeCallback,
-                  testId: testId6,
-                  countRender: countRender6,
+                  testId: testId7,
+                  countRender: countRender7,
                 }}
-              >
+              />
+            </CanUpdate>
+            <CanListen
+              {...{
+                subscribeId: subscribeId1,
+                testId: testId8,
+                countRender: countRender8,
+              }}
+            >
+              <div>
                 <CanListenAndUpdate
                   {...{
-                    subscribeId: subscribeId2,
-                    composeCallback: altComposeCallback,
-                    testId: testId7,
-                    countRender: countRender7,
+                    subscribeId: subscribeId1,
+                    testId: testId9,
+                    countRender: countRender9,
                   }}
                 />
-              </CanUpdate>
-              <CanListen
-                {...{
-                  subscribeId: subscribeId1,
-                  testId: testId8,
-                  countRender: countRender8,
-                }}
-              >
-                <div>
-                  <CanListenAndUpdate
-                    {...{
-                      subscribeId: subscribeId1,
-                      testId: testId9,
-                      countRender: countRender9,
-                    }}
-                  />
-                </div>
-              </CanListen>
-            </div>
+              </div>
+            </CanListen>
           </div>
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 
-    const { unmount, fireNode, getTextFromNode } = render(<TestComponent />);
-    // expect(getLastMaps()).toBe(maps);
-    const maps = getLastMaps();
-    expect(getTextFromNode(testId1)).toBe('');
-    expect(getTextFromNode(testId2)).toBe('');
-    fireNode(testId4, 'i');
-    expect(getTextFromNode(testId1)).toBe('i');
-    expect(getTextFromNode(testId3)).toBe('i');
-    expect(getTextFromNode(testId4)).toBe('i');
-    expect(getTextFromNode(testId8)).toBe('i');
-    expect(getTextFromNode(testId9)).toBe('i');
-    expect(countRender1).toHaveBeenCalledTimes(2);
-    expect(countRender2).toHaveBeenCalledTimes(1);
-    expect(countRender3).toHaveBeenCalledTimes(2);
-    expect(countRender4).toHaveBeenCalledTimes(2);
-    expect(countRender5).toHaveBeenCalledTimes(1);
-    expect(countRender6).toHaveBeenCalledTimes(1);
-    expect(countRender7).toHaveBeenCalledTimes(1);
-    expect(countRender8).toHaveBeenCalledTimes(2);
-    expect(countRender9).toHaveBeenCalledTimes(2);
-    fireNode(testId2, 'j');
-    expect(getTextFromNode(testId2)).toBe('j');
-    expect(getTextFromNode(testId5)).toBe('j');
-    expect(getTextFromNode(testId7)).toBe('j');
-    expect(countRender1).toHaveBeenCalledTimes(2);
-    expect(countRender2).toHaveBeenCalledTimes(2);
-    expect(countRender3).toHaveBeenCalledTimes(2);
-    expect(countRender4).toHaveBeenCalledTimes(2);
-    expect(countRender5).toHaveBeenCalledTimes(2);
-    expect(countRender6).toHaveBeenCalledTimes(1);
-    expect(countRender7).toHaveBeenCalledTimes(2);
-    expect(countRender8).toHaveBeenCalledTimes(2);
-    expect(countRender9).toHaveBeenCalledTimes(2);
-    fireNode(testId9, 'o');
-    expect(getTextFromNode(testId1)).toBe('o');
-    expect(getTextFromNode(testId3)).toBe('o');
-    expect(getTextFromNode(testId4)).toBe('o');
-    expect(getTextFromNode(testId8)).toBe('o');
-    expect(getTextFromNode(testId9)).toBe('o');
-    expect(countRender1).toHaveBeenCalledTimes(3);
-    expect(countRender2).toHaveBeenCalledTimes(2);
-    expect(countRender3).toHaveBeenCalledTimes(3);
-    expect(countRender4).toHaveBeenCalledTimes(3);
-    expect(countRender5).toHaveBeenCalledTimes(2);
-    expect(countRender6).toHaveBeenCalledTimes(1);
-    expect(countRender7).toHaveBeenCalledTimes(2);
-    expect(countRender8).toHaveBeenCalledTimes(3);
-    expect(countRender9).toHaveBeenCalledTimes(3);
-    fireNode(testId7, 'z');
-    expect(getTextFromNode(testId2)).toBe('jz');
-    expect(getTextFromNode(testId5)).toBe('jz');
-    expect(getTextFromNode(testId7)).toBe('jz');
-    expect(countRender1).toHaveBeenCalledTimes(3);
-    expect(countRender2).toHaveBeenCalledTimes(3);
-    expect(countRender3).toHaveBeenCalledTimes(3);
-    expect(countRender4).toHaveBeenCalledTimes(3);
-    expect(countRender5).toHaveBeenCalledTimes(3);
-    expect(countRender6).toHaveBeenCalledTimes(1);
-    expect(countRender7).toHaveBeenCalledTimes(3);
-    expect(countRender8).toHaveBeenCalledTimes(3);
-    expect(countRender9).toHaveBeenCalledTimes(3);
-    expect(maps.map.get(subscribeId1).setters.length).toBe(5);
-    expect(maps.map.get(subscribeId2).setters.length).toBe(3);
-    unmount();
-    expect(maps.map.get(subscribeId1).setters.length).toBe(0);
-    expect(maps.map.get(subscribeId2).setters.length).toBe(0);
+  const { unmount, fireNode, getTextFromNode } = render(<TestComponent />);
+  // expect(getLastMaps()).toBe(maps);
+  const maps = getLastMaps();
+  expect(getTextFromNode(testId1)).toBe('');
+  expect(getTextFromNode(testId2)).toBe('');
+  fireNode(testId4, 'i');
+  expect(getTextFromNode(testId1)).toBe('i');
+  expect(getTextFromNode(testId3)).toBe('i');
+  expect(getTextFromNode(testId4)).toBe('i');
+  expect(getTextFromNode(testId8)).toBe('i');
+  expect(getTextFromNode(testId9)).toBe('i');
+  expect(countRender1).toHaveBeenCalledTimes(2);
+  expect(countRender2).toHaveBeenCalledTimes(1);
+  expect(countRender3).toHaveBeenCalledTimes(2);
+  expect(countRender4).toHaveBeenCalledTimes(2);
+  expect(countRender5).toHaveBeenCalledTimes(1);
+  expect(countRender6).toHaveBeenCalledTimes(1);
+  expect(countRender7).toHaveBeenCalledTimes(1);
+  expect(countRender8).toHaveBeenCalledTimes(2);
+  expect(countRender9).toHaveBeenCalledTimes(2);
+  fireNode(testId2, 'j');
+  expect(getTextFromNode(testId2)).toBe('j');
+  expect(getTextFromNode(testId5)).toBe('j');
+  expect(getTextFromNode(testId7)).toBe('j');
+  expect(countRender1).toHaveBeenCalledTimes(2);
+  expect(countRender2).toHaveBeenCalledTimes(2);
+  expect(countRender3).toHaveBeenCalledTimes(2);
+  expect(countRender4).toHaveBeenCalledTimes(2);
+  expect(countRender5).toHaveBeenCalledTimes(2);
+  expect(countRender6).toHaveBeenCalledTimes(1);
+  expect(countRender7).toHaveBeenCalledTimes(2);
+  expect(countRender8).toHaveBeenCalledTimes(2);
+  expect(countRender9).toHaveBeenCalledTimes(2);
+  fireNode(testId9, 'o');
+  expect(getTextFromNode(testId1)).toBe('o');
+  expect(getTextFromNode(testId3)).toBe('o');
+  expect(getTextFromNode(testId4)).toBe('o');
+  expect(getTextFromNode(testId8)).toBe('o');
+  expect(getTextFromNode(testId9)).toBe('o');
+  expect(countRender1).toHaveBeenCalledTimes(3);
+  expect(countRender2).toHaveBeenCalledTimes(2);
+  expect(countRender3).toHaveBeenCalledTimes(3);
+  expect(countRender4).toHaveBeenCalledTimes(3);
+  expect(countRender5).toHaveBeenCalledTimes(2);
+  expect(countRender6).toHaveBeenCalledTimes(1);
+  expect(countRender7).toHaveBeenCalledTimes(2);
+  expect(countRender8).toHaveBeenCalledTimes(3);
+  expect(countRender9).toHaveBeenCalledTimes(3);
+  fireNode(testId7, 'z');
+  expect(getTextFromNode(testId2)).toBe('jz');
+  expect(getTextFromNode(testId5)).toBe('jz');
+  expect(getTextFromNode(testId7)).toBe('jz');
+  expect(countRender1).toHaveBeenCalledTimes(3);
+  expect(countRender2).toHaveBeenCalledTimes(3);
+  expect(countRender3).toHaveBeenCalledTimes(3);
+  expect(countRender4).toHaveBeenCalledTimes(3);
+  expect(countRender5).toHaveBeenCalledTimes(3);
+  expect(countRender6).toHaveBeenCalledTimes(1);
+  expect(countRender7).toHaveBeenCalledTimes(3);
+  expect(countRender8).toHaveBeenCalledTimes(3);
+  expect(countRender9).toHaveBeenCalledTimes(3);
+  expect(maps.map.get(subscribeId1).setters.length).toBe(5);
+  expect(maps.map.get(subscribeId2).setters.length).toBe(3);
+  unmount();
+  expect(maps.map.get(subscribeId1).setters.length).toBe(0);
+  expect(maps.map.get(subscribeId2).setters.length).toBe(0);
+};
+
+describe('Test useInterstate functionality for primary API', () => {
+  const imports = {};
+
+  beforeEach(() => {
+    jest.isolateModules(() => {
+      // eslint-disable-next-line global-require
+      Object.assign(imports, require('./prerequisite'), require('./testComponentPrimaryAPI'));
+    });
   });
+
+  test('siblings can communicate', siblingsCanCommunicate(imports));
+
+  test('sophisticated structure can communicate', sophisticatedStructure(imports));
+});
+
+describe('Test useInterstate functionality for secondary API', () => {
+  const imports = {};
+
+  beforeEach(() => {
+    jest.isolateModules(() => {
+      // eslint-disable-next-line global-require
+      Object.assign(imports, require('./prerequisite'), require('./testComponentSecondAPI'));
+    });
+  });
+
+  test('siblings can communicate', siblingsCanCommunicate(imports));
+
+  test('sophisticated structure can communicate', sophisticatedStructure(imports));
 });
--- packages/smart-hooks/use-interstate/__tests__/useInterstate.2.test.js
+++ packages/smart-hooks/use-interstate/__tests__/useInterstate.2.test.js
@@ -3,113 +3,124 @@
 
 import React from 'react';
 
-describe('Test useInterstate functionality', () => {
-  let render;
-  let CanListen;
-  let CanUpdate;
-  let CanListenAndUpdate;
+const checkInitializationConcurrency = imports => () => {
+  const {
+    render, CanListen, CanUpdate, CanListenAndUpdate,
+  } = imports;
+  const subscribeId = '1';
+  const testId = 'first';
+
+  const InnerComponent = () => (
+    <div>
+      <CanListen
+        {...{
+          subscribeId,
+          initialValue: 'b',
+        }}
+      />
+    </div>
+  );
+
+  const TestComponent = () => (
+    <>
+      <div>
+        <div>
+          <CanUpdate
+            {...{
+              subscribeId,
+              initialValue: 'a',
+            }}
+          />
+          <InnerComponent />
+        </div>
+      </div>
+      <CanListenAndUpdate
+        {...{
+          subscribeId,
+          testId,
+          initialValue: 'c',
+        }}
+      />
+    </>
+  );
+
+  const { unmount, getTextFromNode } = render(<TestComponent />);
+  expect(getTextFromNode(testId)).toBe('c');
+  unmount();
+};
+
+const valuesRemainAfterTreeUnmount = imports => () => {
+  const { render, CanListen, CanUpdate } = imports;
+  const subscribeId = '1';
+  const testId1 = 'updater';
+  const testId2 = 'listener';
+  const countRender1 = jest.fn();
+  const countRender2 = jest.fn();
+  const altComposeCallback = set => ({ target: { value } }) => {
+    set(old => (old || '') + value);
+  };
+  const TestComponent = () => (
+    <>
+      <CanUpdate
+        {...{
+          subscribeId,
+          testId: testId1,
+          composeCallback: altComposeCallback,
+          countRender: countRender1,
+        }}
+      />
+      <CanListen
+        {...{
+          subscribeId,
+          testId: testId2,
+          countRender: countRender2,
+        }}
+      />
+    </>
+  );
+
+  const {
+    unmount, rerender, fireNode, getTextFromNode,
+  } = render(<TestComponent />);
+  expect(getTextFromNode(testId2)).toBe('');
+  fireNode(testId1, 'g');
+  fireNode(testId1, 'e');
+  expect(getTextFromNode(testId2)).toBe('ge');
+  unmount();
+  rerender(<TestComponent />);
+  expect(getTextFromNode(testId2)).toBe('ge');
+  fireNode(testId1, 'f');
+  fireNode(testId1, 'r');
+  expect(getTextFromNode(testId2)).toBe('gefr');
+  unmount();
+};
+
+describe('Test useInterstate functionality for primary API', () => {
+  const imports = {};
 
   beforeEach(() => {
     jest.isolateModules(() => {
-      ({
-        render,
-        // eslint-disable-next-line global-require
-      } = require('./prerequisite'));
-
-      ({
-        CanListen,
-        CanUpdate,
-        CanListenAndUpdate,
-        // eslint-disable-next-line global-require
-      } = require('./testComponentGeneralAPI'));
+      // eslint-disable-next-line global-require
+      Object.assign(imports, require('./prerequisite'), require('./testComponentPrimaryAPI'));
     });
   });
 
-  test('check initialization concurrency', () => {
-    const subscribeId = '1';
-    const testId = 'first';
+  test('check initialization concurrency', checkInitializationConcurrency(imports));
 
-    const InnerComponent = () => (
-      <div>
-        <CanListen
-          {...{
-            subscribeId,
-            initialValue: 'b',
-          }}
-        />
-      </div>
-    );
+  test('values remain after tree unmount', valuesRemainAfterTreeUnmount(imports));
+});
 
-    const TestComponent = () => (
-      <>
-        <div>
-          <div>
-            <CanUpdate
-              {...{
-                subscribeId,
-                initialValue: 'a',
-              }}
-            />
-            <InnerComponent />
-          </div>
-        </div>
-        <CanListenAndUpdate
-          {...{
-            subscribeId,
-            testId,
-            initialValue: 'c',
-          }}
-        />
-      </>
-    );
+describe('Test useInterstate functionality for secondary API', () => {
+  const imports = {};
 
-    const { unmount, getTextFromNode } = render(<TestComponent />);
-    expect(getTextFromNode(testId)).toBe('c');
-    unmount();
+  beforeEach(() => {
+    jest.isolateModules(() => {
+      // eslint-disable-next-line global-require
+      Object.assign(imports, require('./prerequisite'), require('./testComponentSecondAPI'));
+    });
   });
 
-  test('values remain after tree unmount', () => {
-    const subscribeId = '1';
-    const testId1 = 'updater';
-    const testId2 = 'listener';
-    const countRender1 = jest.fn();
-    const countRender2 = jest.fn();
-    const altComposeCallback = set => ({ target: { value } }) => {
-      set(old => (old || '') + value);
-    };
-    const TestComponent = () => (
-      <>
-        <CanUpdate
-          {...{
-            subscribeId,
-            testId: testId1,
-            composeCallback: altComposeCallback,
-            countRender: countRender1,
-          }}
-        />
-        <CanListen
-          {...{
-            subscribeId,
-            testId: testId2,
-            countRender: countRender2,
-          }}
-        />
-      </>
-    );
+  test('check initialization concurrency', checkInitializationConcurrency(imports));
 
-    const {
-      unmount, rerender, fireNode, getTextFromNode,
-    } = render(<TestComponent />);
-    expect(getTextFromNode(testId2)).toBe('');
-    fireNode(testId1, 'g');
-    fireNode(testId1, 'e');
-    expect(getTextFromNode(testId2)).toBe('ge');
-    unmount();
-    rerender(<TestComponent />);
-    expect(getTextFromNode(testId2)).toBe('ge');
-    fireNode(testId1, 'f');
-    fireNode(testId1, 'r');
-    expect(getTextFromNode(testId2)).toBe('gefr');
-    unmount();
-  });
+  test('values remain after tree unmount', valuesRemainAfterTreeUnmount(imports));
 });
