e9b438fcdf6a9d2807b19510a535608c5fbcc164
Andrei Kovalev <turtlefly@gmail.com>
2019-10-01 18:49:36 -0400

    Add TypeScript to project

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript'],
   env: {
     test: {
       plugins: ['@babel/plugin-transform-runtime'],
--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   verbose: true,
   testRegex: '(src/utils|packages).*/__tests__/.*\\.test\\.[jt]sx?$',
   testPathIgnorePatterns: ['/node_modules/', '.git'],
+  preset: 'ts-jest/presets/js-with-babel',
 };
