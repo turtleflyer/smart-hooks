d7c22e35d5707d0b7db7177e8c7cdd756a32e7b0
Andrei Kovalev <turtlefly@gmail.com>
2019-12-18 03:14:30 -0500

    Add performance validation to test

--- packages/smart-hooks/use-interstate/__tests__/tests/valuesRemainAfterTreeUnmount.tsx
+++ packages/smart-hooks/use-interstate/__tests__/tests/valuesRemainAfterTreeUnmount.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { TestDescription, ComposeCallback } from '../testsAssets';
 
-const valuesRemainAfterTreeUnmount: TestDescription = (p, createTestComponents) => [
+const valuesRemainAfterTreeUnmount: TestDescription = (
+  p,
+  createTestComponents,
+  shouldTestPerformance,
+) => [
   'values remain after tree unmount',
   () => {
     const {
-      assets: { render },
+      assets: { render, getLastMap },
     } = p;
     const { CanListen, CanUpdate } = createTestComponents(p);
     const subscribeId = '1';
@@ -33,13 +37,20 @@ const valuesRemainAfterTreeUnmount: TestDescription = (p, createTestComponents) 
     );
 
     const { unmount, rerender, fireNode, getTextFromNode } = render(<TestComponent />);
+    const map = getLastMap();
     expect(getTextFromNode(testId2)).toBe('');
 
     fireNode(testId1, 'g');
     fireNode(testId1, 'e');
     expect(getTextFromNode(testId2)).toBe('ge');
+    if (shouldTestPerformance) {
+      expect((map.get(subscribeId) as { setters: any[] }).setters.length).toBe(1);
+    }
 
     unmount();
+    if (shouldTestPerformance) {
+      expect((map.get(subscribeId) as { setters: any[] }).setters.length).toBe(0);
+    }
 
     rerender(<TestComponent />);
     expect(getTextFromNode(testId2)).toBe('ge');
@@ -47,8 +58,14 @@ const valuesRemainAfterTreeUnmount: TestDescription = (p, createTestComponents) 
     fireNode(testId1, 'f');
     fireNode(testId1, 'r');
     expect(getTextFromNode(testId2)).toBe('gefr');
+    if (shouldTestPerformance) {
+      expect((map.get(subscribeId) as { setters: any[] }).setters.length).toBe(1);
+    }
 
     unmount();
+    if (shouldTestPerformance) {
+      expect((map.get(subscribeId) as { setters: any[] }).setters.length).toBe(0);
+    }
   },
 ];
 
