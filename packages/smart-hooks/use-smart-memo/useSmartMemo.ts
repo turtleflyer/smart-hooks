6e48868f8722a6bc2395ce5d3d6a7c3998ec588a
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-01-16 03:01:15 -0500

    Merge two modules

--- packages/smart-hooks/use-smart-memo/useSmartMemo.ts
+++ packages/smart-hooks/use-smart-memo/useSmartMemo.ts
@@ -1,12 +1,32 @@
-import { useMemo } from 'react';
-import {
-  useMaintainMemoStore,
-  recalculateConditionally,
-} from './recalculateConditionally';
+import { useMemo, useRef } from 'react';
+
+interface MemoStore<T> {
+  deps?: ReadonlyArray<any>;
+  memValue?: T;
+}
 
 function useSmartMemo<T>(factory: () => T, deps: ReadonlyArray<any>): T {
-  const store = useMaintainMemoStore<T>();
-  return useMemo<T>(() => recalculateConditionally(factory, deps, store), deps);
+  const { current: store } = useRef<MemoStore<T>>({});
+  return useMemo<T>(() => {
+    const { deps: prevDeps } = store;
+    let gottaRecalculate = !prevDeps || prevDeps.length !== deps.length;
+
+    for (
+      let i = 0;
+      !gottaRecalculate && i < (prevDeps as ReadonlyArray<any>).length;
+      i++
+    ) {
+      gottaRecalculate = !Object.is((prevDeps as any[])[i], deps[i]);
+    }
+
+    if (gottaRecalculate) {
+      const memValue = factory();
+      Object.assign(store, { deps, memValue });
+      return memValue;
+    }
+
+    return store.memValue as T;
+  }, deps);
 }
 
 export { useSmartMemo };
