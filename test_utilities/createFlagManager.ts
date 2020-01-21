3a81a39a24e504d7c47aa3c24a6deb4267310f1c
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-01-21 18:33:55 -0500

    Extract FlagManager interface

--- test_utilities/createFlagManager.ts
+++ test_utilities/createFlagManager.ts
@@ -1,17 +1,23 @@
-function createFlagManager<T extends {}>(flags: T, def: T) {
+interface FlagManager<T> {
+  reset: () => void;
+  set: <F extends keyof T>(flag: F, value: T[F]) => void;
+  read: <F extends keyof T>(flag: F) => void;
+}
+
+function createFlagManager<T extends {}>(flags: T, def: T): FlagManager<T> {
   return {
     reset() {
       Object.assign(flags, def);
     },
 
-    set<F extends keyof T>(flag: F, value: T[F]) {
+    set(flag, value) {
       flags[flag] = value;
     },
 
-    read<F extends keyof T>(flag: F) {
+    read(flag) {
       return flags[flag];
     },
   };
 }
 
-export { createFlagManager };
+export { createFlagManager, FlagManager };
