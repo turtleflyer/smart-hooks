b7b473dce38971586ce51fe8f7f991624fb8239d
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-01-21 18:47:12 -0500

    Fix 'read' property should return type T[F]

--- test_utilities/createFlagManager.ts
+++ test_utilities/createFlagManager.ts
@@ -1,7 +1,7 @@
 interface FlagManager<T> {
   reset: () => void;
   set: <F extends keyof T>(flag: F, value: T[F]) => void;
-  read: <F extends keyof T>(flag: F) => void;
+  read: <F extends keyof T>(flag: F) => T[F];
 }
 
 function createFlagManager<T extends {}>(flags: T, def: T): FlagManager<T> {
