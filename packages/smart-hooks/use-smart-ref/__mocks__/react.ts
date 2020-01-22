965b97e7eebc40d16d2068b8259a94c7eef09a9e
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-01-22 02:23:52 -0500

    Revert "Test useSmartRef bypassing useMemo"
    This reverts commit a6cfa7b510a2b74472279c864f7cc03d44996ee4.

--- packages/smart-hooks/use-smart-ref/__mocks__/react.ts
+++
@@ -1,6 +0,0 @@
-import { flagManager } from '../__tests__/testFlags';
-import { mockReactUseMemo } from '../../../../test_utilities/mockReactUseMemo';
-
-const react = jest.requireActual('react');
-
-module.exports = mockReactUseMemo(react, flagManager);
--- packages/smart-hooks/use-smart-ref/__tests__/mainTestSuit.ts
+++ packages/smart-hooks/use-smart-ref/__tests__/mainTestSuit.ts
@@ -3,33 +3,14 @@ import { AssetsImport, UseSmartRefImport, TestParameter } from './testsAssets';
 import cleaningAfterUnmount from './tests/cleaningAfterUnmount';
 import testOmittingOptionalAPI from './tests/testOmittingOptionalAPI';
 import typeParameter from './tests/typeParameter';
-import { flagManager } from './testFlags';
 
 const mainTestSuit = (packagePath: string) =>
-  describe.each([
-    [
-      'using original useMemo',
-      () => flagManager.set('MOCK_USE_MEMO', false),
-      'original',
-    ],
-    [
-      'using mocked useMemo',
-      () => flagManager.set('MOCK_USE_MEMO', true),
-      'mocked',
-    ],
-  ])('Test useSmartRef correctness (%s)', (name, setMock, proofOfMock) => {
+  describe('Test useSmartRef correctness', () => {
     const testParameter: TestParameter = {} as TestParameter;
 
-    beforeAll(() => {
-      flagManager.set('PROOF_OF_MOCK', '');
-    });
-
     beforeEach(() => {
-      setMock();
       jest.isolateModules(() => {
-        const {
-          executionCountersFactory,
-        } = require('./testsAssets') as AssetsImport;
+        const { executionCountersFactory } = require('./testsAssets') as AssetsImport;
         const { useSmartRef } = require(packagePath) as UseSmartRefImport;
         testParameter.assets = { executionCountersFactory, useSmartRef };
       });
@@ -38,10 +19,6 @@ const mainTestSuit = (packagePath: string) =>
     test(...cleaningAfterUnmount(testParameter));
     test(...testOmittingOptionalAPI(testParameter));
     test(...typeParameter(testParameter));
-
-    test('proof of mock', () => {
-      expect(flagManager.read('PROOF_OF_MOCK')).toBe(proofOfMock);
-    });
   });
 
 export default mainTestSuit;
