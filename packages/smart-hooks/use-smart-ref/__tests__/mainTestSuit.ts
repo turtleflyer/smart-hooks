a6cfa7b510a2b74472279c864f7cc03d44996ee4
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-01-21 22:47:54 -0500

    Test useSmartRef bypassing useMemo

--- packages/smart-hooks/use-smart-ref/__tests__/mainTestSuit.ts
+++ packages/smart-hooks/use-smart-ref/__tests__/mainTestSuit.ts
@@ -3,14 +3,33 @@ import { AssetsImport, UseSmartRefImport, TestParameter } from './testsAssets';
 import cleaningAfterUnmount from './tests/cleaningAfterUnmount';
 import testOmittingOptionalAPI from './tests/testOmittingOptionalAPI';
 import typeParameter from './tests/typeParameter';
+import { flagManager } from './testFlags';
 
 const mainTestSuit = (packagePath: string) =>
-  describe('Test useSmartRef correctness', () => {
+  describe.each([
+    [
+      'using original useMemo',
+      () => flagManager.set('MOCK_USE_MEMO', false),
+      'original',
+    ],
+    [
+      'using mocked useMemo',
+      () => flagManager.set('MOCK_USE_MEMO', true),
+      'mocked',
+    ],
+  ])('Test useSmartRef correctness (%s)', (name, setMock, proofOfMock) => {
     const testParameter: TestParameter = {} as TestParameter;
 
+    beforeAll(() => {
+      flagManager.set('PROOF_OF_MOCK', '');
+    });
+
     beforeEach(() => {
+      setMock();
       jest.isolateModules(() => {
-        const { executionCountersFactory } = require('./testsAssets') as AssetsImport;
+        const {
+          executionCountersFactory,
+        } = require('./testsAssets') as AssetsImport;
         const { useSmartRef } = require(packagePath) as UseSmartRefImport;
         testParameter.assets = { executionCountersFactory, useSmartRef };
       });
@@ -19,6 +38,10 @@ const mainTestSuit = (packagePath: string) =>
     test(...cleaningAfterUnmount(testParameter));
     test(...testOmittingOptionalAPI(testParameter));
     test(...typeParameter(testParameter));
+
+    test('proof of mock', () => {
+      expect(flagManager.read('PROOF_OF_MOCK')).toBe(proofOfMock);
+    });
   });
 
 export default mainTestSuit;
---
+++ packages/smart-hooks/use-smart-ref/__tests__/testFlags.ts
@@ -0,0 +1,19 @@
+import { createFlagManager } from '../../../../test_utilities/createFlagManager';
+
+interface TestFlags {
+  MOCK_USE_MEMO: boolean;
+  PROOF_OF_MOCK: string;
+}
+
+const testFlagsDefault: TestFlags = {
+  MOCK_USE_MEMO: false,
+  PROOF_OF_MOCK: '',
+};
+
+const testFlags = {} as TestFlags;
+
+const flagManager = createFlagManager(testFlags, testFlagsDefault);
+
+flagManager.reset();
+
+export { flagManager };
