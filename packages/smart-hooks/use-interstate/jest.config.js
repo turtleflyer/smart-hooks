b1a489ec791f064640ecad327d7c7b8ab7ec1aaf
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-05-21 01:27:09 -0700

    Make tsconfig compatible with React configuration while performing tests

--- packages/smart-hooks/use-interstate/jest.config.js
+++ packages/smart-hooks/use-interstate/jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   testRegex: '__tests__/.*\\.test\\.[jt]sx?$',
   testPathIgnorePatterns: ['/node_modules/', '.git'],
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
 };
