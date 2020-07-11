e1a19f8db41cf491c99ca7099800e77e5a100620
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-07-10 17:26:15 -0700

    Make useMultistate returns object with readonly members

--- packages/smart-hooks/use-multi-state/src/useMultistate.ts
+++ packages/smart-hooks/use-multi-state/src/useMultistate.ts
@@ -1,10 +1,16 @@
 import React, { useMemo, useRef, useState } from 'react';
 
 export type SettersObject<S extends object> = {
-  [P in keyof S]: React.Dispatch<React.SetStateAction<S[P]>>;
+  readonly [P in keyof S]: React.Dispatch<React.SetStateAction<S[P]>>;
 };
 
-export function useMultiState<S extends object>(stateScheme: S): [S, SettersObject<S>] {
+export type StateObject<S extends object> = { readonly [P in keyof S]: S[P] };
+
+type UnsealReadOnly<R extends object> = { [P in keyof R]: R[P] };
+
+export function useMultiState<S extends object>(
+  stateScheme: S
+): [StateObject<S>, SettersObject<S>] {
   const [memStateScheme] = useState(stateScheme);
 
   const enumKeys = useMemo(
@@ -18,7 +24,7 @@ export function useMultiState<S extends object>(stateScheme: S): [S, SettersObje
   ) as Array<keyof S>;
 
   let stateObject = {} as S;
-  const { current: settersObject } = useRef({} as SettersObject<S>);
+  const { current: settersObject } = useRef({} as UnsealReadOnly<SettersObject<S>>);
 
   for (const key of enumKeys) {
     const [state, setter] = useState(memStateScheme[key]);
@@ -26,5 +32,5 @@ export function useMultiState<S extends object>(stateScheme: S): [S, SettersObje
     settersObject[key] = setter;
   }
 
-  return [stateObject, settersObject];
+  return [stateObject as StateObject<S>, settersObject as SettersObject<S>];
 }
