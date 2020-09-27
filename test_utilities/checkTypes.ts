623a0b10398581ab10079a9efb2bbedee50ab402
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-09-27 16:15:42 -0700

    Enhance types testing tools

--- test_utilities/checkTypes.ts
+++ test_utilities/checkTypes.ts
@@ -1,7 +1,8 @@
 export type Reveal<T extends never> = T;
 
-export type ToBeExact<T1, T2> = ((a: T1) => T1) extends (a: T2) => T2
-  ? ((a: keyof T1) => keyof T1) extends (a: keyof T2) => keyof T2
-    ? never
-    : unknown
+export type ToBeExact<T1, T2> = ((a: T1, b: keyof T1) => [T1, keyof T1]) extends (
+  a: T2,
+  b: keyof T2
+) => [T2, keyof T2]
+  ? never
   : unknown;
